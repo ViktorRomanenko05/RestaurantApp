ed45b5e5735655748458fde49b5d903832b3a626
yamchytskyi <wk.vitalii@gmail.com>
2025-04-22 17:40:28 +0200

    quick fix to allow anybody download menu

--- src/main/java/de/ait/restaurantapp/config/SecurityConfig.java
+++ src/main/java/de/ait/restaurantapp/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
                                 "/restaurant", "/restaurant/",
                                 "/restaurant/reservations/**",
                                 "/restaurant/reserve/**",
-                                "/restaurant/cancel/**"
+                                "/restaurant/cancel/**",
+                                "/restaurant/menu", "/restaurant/menu/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
